b3b199156359232c508af8843242b51215eb5f26
antoniolvlindo <antoniolvitall@gmail.com>
2025-01-09 17:11:17 -0300

    feat: :ambulance: alteracao de rota

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import UserManagement from './Pages/UserManagement/UserManagement'
 import UserDetails from './Pages/UserManagement/UserDetails'
-import SignupForm from './components/SignupForm/SignupForm'
 import SignupPage from './Pages/UserManagement/UserSignup'
 import Login from './Pages/Login/Login'
 
@@ -10,12 +9,11 @@ import Login from './Pages/Login/Login'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <UserManagement /> }/>
+      <Route path="/" element={ <Login /> }/>
       <Route path="/UserManagement" element={ <UserManagement /> }/>
       <Route path="/UserManagement/details/:id" element={ <UserDetails/> }/>
       <Route path="/signup" element={ <SignupPage/> }/>
-       <Route path="/login" element={ <Login /> }/>
-        </Routes>
+    </Routes>
   )
 }
 
